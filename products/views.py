e6c57b6f6b54fc13ea0c7a37d37f27dcaaff57b6
shyamkrishnank <shyamkrishnank18@gmail.com>
2023-08-07 09:45:35 +0530

    image cropping done in backend

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from category.models import Category
 from .models import Products,ProductImage,Varient
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image as PILImage
+import io
 
 # Create your views here.
 def products(request):
@@ -32,13 +35,64 @@ def add_product(request,id):
             for i in Varient.choice:
                 if i[1] in var:
                     Varient.objects.create(product = current, varient = i[0])               
-            ProductImage.objects.create(product = current,image = image1)
-            ProductImage.objects.create(product = current,image = image2)
-            ProductImage.objects.create(product = current,image = image3)
+            
+            image_fields = [image1, image2, image3]
 
+            for idx, image_field in enumerate(image_fields):
 
-            return redirect('product_details',id)
+                if image_field:
+                    pil_image = PILImage.open(image_field)
 
+                    # Define the desired crop ratio (e.g., 2:3)
+                    crop_ratio = (2, 3)
+
+                    # Get the width and height of the original image
+                    width, height = pil_image.size
+
+                    # Set default values for new_width and new_height
+                    new_width, new_height = width, height
+
+                    # Calculate the aspect ratio of the original image
+                    original_ratio = width / height
+
+                    # Calculate the new dimensions to achieve the 2:3 aspect ratio
+                    if original_ratio > crop_ratio[0] / crop_ratio[1]:
+                        new_width = int(height * crop_ratio[0] / crop_ratio[1])
+                    else:
+                        new_height = int(width * crop_ratio[1] / crop_ratio[0])
+
+                    # Calculate the cropping box to center the image
+                    left = (width - new_width) // 2
+                    top = (height - new_height) // 2
+                    right = left + new_width
+                    bottom = top + new_height
+
+                    # Crop the image
+                    cropped_image = pil_image.crop((left, top, right, bottom))
+
+                    print(f"Width: {width}, Height: {height}")
+                    print(f"New Width: {new_width}, New Height: {new_height}")
+                    print(f"Cropping Box: left={left}, top={top}, right={right}, bottom={bottom}")
+
+                    # Save the cropped image as a file-like object
+                    cropped_image_io = io.BytesIO()
+                    image_format = image_field.content_type.split('/')[-1].lower()  # Get the file extension from content type
+                    cropped_image.save(cropped_image_io, format=image_format)
+
+                    # Create a SimpleUploadedFile from the cropped image data
+                    cropped_image_file = SimpleUploadedFile(
+                        name=f"{image_field.name}.{image_format}",
+                        content=cropped_image_io.getvalue(),
+                        content_type=image_field.content_type
+                    )
+
+                    # Create a ProductImage instance and save it to the database
+                    product_image = ProductImage(product=current, image=cropped_image_file)
+                    product_image.save()
+
+            return redirect('product_details', id=id)
+           
+                        
 def delete_product(request,id):
     if 'admin_username' in request.session:
         product = Products.objects.get(id=id)
